e66e33de368356a4a6620f909abf4c33dd89328b
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2017-08-29 11:10:09 +0200

    #6 Move ratepay script

--- includes/external/wirecardcheckoutpage/Payment.php
+++ includes/external/wirecardcheckoutpage/Payment.php
@@ -281,6 +281,7 @@ class WirecardCheckoutPagePayment
         }
         $info = $this->_logo;
         $info .= sprintf('<div class="errormessage" style="display: none;" id="%s_messagebox"></div>', $this->code);
+        $info .= include_once DIR_FS_EXTERNAL . 'wirecardcheckoutpage/includes/ratepay.php';
         $c = strtoupper($this->code);
 
         unset($_SESSION['wcp_financialinstitution']);
@@ -310,22 +311,6 @@ class WirecardCheckoutPagePayment
      */
     public function pre_confirmation_check()
     {
-	    $config = $this->_page->_getConfigArray();
-	    $customer_id = $config['CUSTOMER_ID'];
-
-	    if( isset( $_SESSION['wcp-consumerDeviceId'] ) ) {
-		    $consumerDeviceId = $_SESSION['wcp-consumerDeviceId'];
-	    } else {
-		    $timestamp = microtime();
-		    $consumerDeviceId = md5( $customer_id . "_" . $timestamp );
-		    $_SESSION['wcp-consumerDeviceId'] = $consumerDeviceId;
-	    }
-	    $ratepay = '<script language="JavaScript">var di = {t:"' . $consumerDeviceId . '",v:"WDWL",l:"Checkout"};</script>';
-	    $ratepay .= '<script type="text/javascript" src="//d.ratepay.com/' . $consumerDeviceId . '/di.js"></script>';
-	    $ratepay .= '<noscript><link rel="stylesheet" type="text/css" href="//d.ratepay.com/di.css?t=' . $consumerDeviceId . '&v=WDWL&l=Checkout"></noscript>';
-	    $ratepay .= '<object type="application/x-shockwave-flash" data="//d.ratepay.com/WDWL/c.swf" width="0" height="0"><param name="movie" value="//d.ratepay.com/WDWL/c.swf" /><param name="flashvars" value="t=' . $consumerDeviceId . '&v=WDWL"/><param name="AllowScriptAccess" value="always"/></object>';
-	    echo $ratepay;
-
         return false;
     }
 
